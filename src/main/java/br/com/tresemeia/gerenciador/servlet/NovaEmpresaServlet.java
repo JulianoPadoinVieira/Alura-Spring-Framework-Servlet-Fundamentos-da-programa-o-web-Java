aef4497341b1a6f3a075ef8a0ea1c6f649ac4329
Juliano Padoin Vieira <proprietyofkurd@hotmail.com>
2021-10-15 20:32:06 -0300

    Parte 3 - Definindo nosso modelo

--- src/main/java/br/com/tresemeia/gerenciador/servlet/NovaEmpresaServlet.java
+++ src/main/java/br/com/tresemeia/gerenciador/servlet/NovaEmpresaServlet.java
@@ -18,9 +18,17 @@ public class NovaEmpresaServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	//Podemos substituir o "service" por doGet ou doPost para permitir apenas um tipo de requisição
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
 		System.out.println("Cadastrando nova empresa.");
+		
 		//"nome" é a variável pega da url(http://localhost:8080/gerenciador/novaEmpresa?nome=Atos)
 		String nomeEmpresa = request.getParameter("nome");
+		Empresa empresa = new Empresa();
+		empresa.setNome(nomeEmpresa);
+		
+		Banco banco = new Banco();
+		banco.adiciona(empresa);
+		
 		PrintWriter out = response.getWriter();
 		out.println("<html><body><h1>Empresa " + nomeEmpresa + " cadastrada com sucesso!!!</body><html>");
 	}
